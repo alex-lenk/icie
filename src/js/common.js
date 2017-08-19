1682128b408b323ebd6e338eb9aec455de332d4c
artlenk.ru@gmail.com <df5ukoi7ht>
2017-08-19 17:58:19 +0300

    Added fonts family

--- src/js/common.js
+++ src/js/common.js
@@ -1,19 +1,14 @@
 $(document).ready(function () {
-
-    $(".main-nav-toggle").click(
-        function () {
-            $('.main-nav').toggleClass("menu-visible");
-        }
-    );
-
-    $('.slider').slick({
-        autoplay: true,
-        autoplaySpeed: 4000,
-        prevArrow: '<div data-role="none" class="slick-prev" aria-label="Previous" tabindex="0"><span><svg class="arrow"><use xlink:href="theme/img/sprite.svg#arrow"></use></svg></span></div>',
-        nextArrow: '<div data-role="none" class="slick-next" aria-label="Next" tabindex="0"><span><svg class="arrow"><use xlink:href="theme/img/sprite.svg#arrow"></use></svg></span></div>'
-    });
-
-    jQuery(function($){
-        $(".phone-mask").mask("+7(999) 999-9999");
-    });
+// Copyright 2014-2017 The Bootstrap Authors
+// Copyright 2014-2017 Twitter, Inc.
+// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement('style')
+        msViewportStyle.appendChild(
+            document.createTextNode(
+                '@-ms-viewport{width:auto!important}'
+            )
+        );
+        document.head.appendChild(msViewportStyle)
+    }
 });
