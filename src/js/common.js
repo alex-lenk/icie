a7343d7b0b26d87fa8a10ec04799a87763c631b5
artlenk.ru@gmail.com <df5ukoi7ht>
2017-08-20 08:49:28 +0300

    Created nav block

--- src/js/common.js
+++ src/js/common.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 // Copyright 2014-2017 Twitter, Inc.
 // Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
     if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-        var msViewportStyle = document.createElement('style')
+        var msViewportStyle = document.createElement('style');
         msViewportStyle.appendChild(
             document.createTextNode(
                 '@-ms-viewport{width:auto!important}'
@@ -11,4 +11,16 @@ $(document).ready(function () {
         );
         document.head.appendChild(msViewportStyle)
     }
+
+    $(".search-top-icon").click(
+        function () {
+            $('.search-top').toggleClass("search-top-opened");
+        }
+    );
+
+    $(".language-bar-current").click(
+        function () {
+            $('.language-bar').toggleClass("language-bar-opened");
+        }
+    );
 });
