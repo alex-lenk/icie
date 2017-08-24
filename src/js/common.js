f33d3ae56393bb8ce81f1e4879e96a4ac570d55f
artlenk.ru@gmail.com <df5ukoi7ht>
2017-08-24 13:27:00 +0300

    Created word-block pages

--- src/js/common.js
+++ src/js/common.js
@@ -62,27 +62,29 @@ $(document).ready(function () {
     $(".last-news-text").dotdotdot();
 
 
+    /* BEGIN: Плавающий блок в сайдбаре */
     var
-        $window = $(window), // Основное окно
-
+        $window = $(window), //Основное окно
         $sidebar = $(".sidebar"), // Блок, который нужно фиксировать при прокрутке
-        $sidebarMenu = $(".sidebar-menu"), // Блок, который нужно фиксировать при прокрутке
+        $sidebarMenu = $(".sidebar-menu"); // Блок, который нужно фиксировать при прокрутке
 
+    if ($sidebar.length) {
         $h = $sidebar.offset().top; // Определяем координаты верха нужного блока (например, с навигацией или виджетом, который надо фиксировать)
 
-    $window.on('scroll', function () {
+        $window.on('scroll', function () {
+            // Как далеко вниз прокрутили страницу
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-        // Как далеко вниз прокрутили страницу
-        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            // Если прокрутили скролл ниже макушки нужного блока, включаем ему фиксацию
+            if (scrollTop > $h) {
 
-        // Если прокрутили скролл ниже макушки нужного блока, включаем ему фиксацию
-        if (scrollTop > $h) {
-
-            $sidebarMenu.addClass("fixed");
-            // Иначе возвращаем всё назад
-        } else {
-            $sidebarMenu.removeClass("fixed");
-        }
-    });
+                $sidebarMenu.addClass("fixed");
+                // Иначе возвращаем всё назад
+            } else {
+                $sidebarMenu.removeClass("fixed");
+            }
+        });
+    }
+    /* END */
 
 });
