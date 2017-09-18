c31cb3a704d3041ee07e17b6a3d0c398fabd5af6
artlenk.ru@gmail.com <df5ukoi7ht>
2017-09-18 09:48:07 +0300

    Refactoring our-project block, common.js

--- src/js/common.js
+++ src/js/common.js
@@ -50,7 +50,7 @@ $(document).ready(function () {
 
 
     // BEGIN: Инициализация слайдера на главной странице в блоке партнеров
-    var swiper = new Swiper('.partners-carousel', {
+    var partnersSwiper = new Swiper('.partners-carousel', {
         paginationClickable: true,
         slidesPerView: 6,
         spaceBetween: 50,
@@ -73,32 +73,35 @@ $(document).ready(function () {
 
 
     // BEGIN: Инициализация слайдера на внутренних страницах
-    var swiper = new Swiper('.gallery', {
-        paginationClickable: true,
-        slidesPerView: 4,
-        spaceBetween: 30,
-        grabCursor: true,
-        nextButton: '.gallery-next',
-        prevButton: '.gallery-prev',
-        breakpoints: {
-            1199: {
-                slidesPerView: 5,
-                spaceBetween: 20
-            },
-            992: {
-                slidesPerView: 3,
-                spaceBetween: 20
-            },
-            570: {
-                spaceBetween: 20,
-                slidesPerView: 'auto',
-                centeredSlides: true,
-                grabCursor: true,
-                autoplay: 4000,
-                autoplayDisableOnInteraction: false
+    var $gallery = $(".gallery");
+    if ($gallery.length) {
+        var gallerySwiper = new Swiper('.gallery', {
+            paginationClickable: true,
+            slidesPerView: 4,
+            spaceBetween: 30,
+            grabCursor: true,
+            nextButton: '.gallery-next',
+            prevButton: '.gallery-prev',
+            breakpoints: {
+                1199: {
+                    slidesPerView: 5,
+                    spaceBetween: 20
+                },
+                992: {
+                    slidesPerView: 3,
+                    spaceBetween: 20
+                },
+                570: {
+                    spaceBetween: 20,
+                    slidesPerView: 'auto',
+                    centeredSlides: true,
+                    grabCursor: true,
+                    autoplay: 4000,
+                    autoplayDisableOnInteraction: false
+                }
             }
-        }
-    });
+        });
+    }
     // END
 
 
@@ -207,5 +210,82 @@ $(document).ready(function () {
     );
     //END
 
+    // функция проверки полной видимости элемента
+    var gridMore = $('.grid-more');
+    if (gridMore.length) {
+        function checkPosition() {
 
+            // координаты дива
+            var div_position = gridMore.offset();
+            // отступ сверху
+            var div_top = div_position.top;
+            // отступ слева
+            var div_left = div_position.left;
+            // ширина
+            var div_width = gridMore.width();
+            // высота
+            var div_height = gridMore.height();
+
+            // проскроллено сверху
+            var top_scroll = $(document).scrollTop();
+            // проскроллено слева
+            var left_scroll = $(document).scrollLeft();
+            // ширина видимой страницы
+            var screen_width = $(window).width();
+            // высота видимой страницы
+            var screen_height = $(window).height();
+
+            // координаты углов видимой области
+            var see_x1 = left_scroll;
+            var see_x2 = screen_width + left_scroll;
+            var see_y1 = top_scroll;
+            var see_y2 = screen_height + top_scroll;
+
+            // координаты углов искомого элемента
+            var div_x1 = div_left;
+            var div_x2 = div_left + div_height;
+            var div_y1 = div_top;
+            var div_y2 = div_top + div_width;
+
+            // проверка - виден див полностью или нет
+            if (div_x1 >= see_x1 && div_x2 <= see_x2 && div_y1 >= see_y1 && div_y2 <= see_y2) {
+                // если виден
+                gridMore.addClass('grid-more-left');
+                gridMore.removeClass('grid-more-right');
+            } else {
+                // если не виден
+                gridMore.addClass('grid-more-right');
+                gridMore.removeClass('grid-more-left');
+            }
+        }
+
+        $(document).scroll(function () {
+            // при скролле страницы делаем проверку
+            checkPosition();
+        });
+
+        // после загрузки страницы сразу проверяем
+        checkPosition();
+
+        // проверка при масштабировании и изменении размера страницы
+        $(window).resize(function () {
+            checkPosition();
+        });
+    }
+    //END
+
+
+    // BEGIN: Инициализация слайдера на главной страницы
+    var $ourProjectList = $(".our-project-list"),
+        $windowInnerWidth = window.innerWidth;
+
+
+    if ($windowInnerWidth < 568 && $ourProjectList.length) {
+        var ourProjectListSwiper = new Swiper('.our-project-list', {
+            slidesPerView: 'auto',
+            centeredSlides: true,
+            grabCursor: true
+        });
+    }
+    // END
 });
